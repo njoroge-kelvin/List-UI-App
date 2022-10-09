7e10c4d8c586df6e1290338910669a01b03034df
njoroge-kelvin <kelvinnjoroge423@gmail.com>
2022-10-09 13:28:40 +0300

    New ListTile dialog update

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,10 @@ void main() {
   ));
 }
 
-enum Menu {monday, tuesday, thursday}
+enum Menu { monday, tuesday, thursday }
 
 const TextStyle menuStyle = TextStyle(color: Colors.black, fontSize: 13.0);
+
 
 class ListApp extends StatefulWidget {
   const ListApp({Key? key}) : super(key: key);
@@ -32,7 +33,7 @@ class _ListAppState extends State<ListApp> {
             Tab(
               icon: Icon(size: 20.0, Icons.account_balance_rounded),
               child: PopupMenuButton(
-                onSelected: (Menu item){
+                onSelected: (Menu item) {
                   setState(() {
                     _selectedMenu = item.name;
                   });
@@ -114,59 +115,108 @@ class _ListAppState extends State<ListApp> {
           ]),
           ListView(scrollDirection: Axis.vertical, children: [
             for (var i = 0; i < 101; i++)
-              Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
                       color: Colors.teal,
                       borderRadius: BorderRadius.circular(15),
-                      boxShadow: [BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3),
-                      ),]),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 25,
-                        backgroundImage: AssetImage('assets/images/Image1.jpg'),
-                        child: GestureDetector(
-                          onTap: () async {
-                            await showDialog(context: context, builder: (_) => ItemList());
-                        },
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.8),
+                          spreadRadius: 2,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
                         ),
-                      ),
-                      title: Text('Leading TextTile'),
-                      subtitle: Text('This is a TextTile widget'),
-                      trailing: Icon(Icons.ac_unit_outlined),
+                      ]),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 25,
+                      backgroundImage: AssetImage('assets/images/Image1.jpg'),
+                      child: GestureDetector(
+                        onTap: () async {
+                          await showDialog(
+                              context: context, builder: (_) => ItemList());
+                        },
                       ),
                     ),
-                  SizedBox(
-                    height: 5.0,
+                    title: Text('Leading TextTile'),
+                    subtitle: Text('This is a TextTile widget'),
+                    trailing: Icon(Icons.ac_unit_outlined),
                   ),
-                ],
+                ),
               ),
           ])
         ]));
   }
 }
 
-class ItemList extends StatelessWidget {
+class ItemList extends StatefulWidget {
   const ItemList({Key? key}) : super(key: key);
+
+  @override
+  State<ItemList> createState() => _ItemListState();
+}
+
+class _ItemListState extends State<ItemList> {
+  int _selectedIndex = 0;
+
+  _onItemTapped(int index){
+    setState((){
+      _selectedIndex = index;
+    },);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      child: Container(
-          width: 100.0,
-          height: 200.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-            image: DecorationImage(
-              image: ExactAssetImage('assets/images/Image1.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SizedBox(
+          width: 300,
+          height: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Icon(Icons.spatial_audio),
+                  Text('This is your profile',),
+                  CircleAvatar(radius: 15,),
+                ],
+              ),
+              Text('Lorem Ipsum is simply dummy text of the printing '
+                  'and typesetting industry. Lorem Ipsum has been the industrys '
+                  'standard dummy text ever since the 1500s, when an unknown printer but also the leap into electronic '
+                  'typesetting, remaining essentially unchanged. It was popularised in the '
+                 ),
+              BottomNavigationBar(
+                selectedItemColor: Colors.greenAccent,
+                elevation: 1,
+                onTap: _onItemTapped,
+                currentIndex : _selectedIndex,
+                items: const[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.business),
+                  label: 'Business',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.school),
+                  label: 'School',
+                ),
+              ],)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+
+
